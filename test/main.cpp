e11f54dd9f1e7fb7c4dac7711032c362d0a9a44a
Adam <9024049+aaddaammgg@users.noreply.github.com>
2022-02-26 17:31:09 -0800

    REFACTOR: Fixed incorrect project names

--- test/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <ctime>
-
-int main() {
-    std::cout << std::time(0) << std::endl;
-    std::cout << std::time(NULL) << std::endl;
-    std::cout << std::time(nullptr) << std::endl;
-    return 0;
-}
