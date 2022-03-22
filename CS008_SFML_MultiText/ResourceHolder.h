ca6786a6e48345fcc1f5059f37d886d639184fe5
Adam <9024049+aaddaammgg@users.noreply.github.com>
2022-03-21 18:49:07 -0700

    edited sfml_template resourceholder -> ifndef

--- CS008_SFML_MultiText/ResourceHolder.h
+++ CS008_SFML_MultiText/ResourceHolder.h
@@ -2,8 +2,8 @@
 // Created by Adam G. on 11/20/2021.
 // https://en.sfml-dev.org/forums/index.php?topic=19928.0
 
-#ifndef CS3A_KINGS_CORNER_RESOURCEHOLDER_H
-#define CS3A_KINGS_CORNER_RESOURCEHOLDER_H
+#ifndef RESOURCEHOLDER_H
+#define RESOURCEHOLDER_H
 
 #include <SFML/Graphics.hpp>
 #include <map>
@@ -21,4 +21,4 @@ public:
 };
 
 
-#endif //CS3A_KINGS_CORNER_RESOURCEHOLDER_H
+#endif //RESOURCEHOLDER_H
