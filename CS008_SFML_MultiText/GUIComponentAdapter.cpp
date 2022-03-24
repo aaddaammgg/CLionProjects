60cb46c4d6561bc565fc2ee525fc01d2cccddd18
Adam <9024049+aaddaammgg@users.noreply.github.com>
2022-03-23 23:51:46 -0700

    GUIComponentAdapter -> if you do not want to use certain virtual functions

---
+++ CS008_SFML_MultiText/GUIComponentAdapter.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Adam G. on 3/23/22.
+//
+
+#include "GUIComponentAdapter.h"
+
+void GUIComponentAdapter::draw(sf::RenderTarget &window, sf::RenderStates states) const {
+
+}
+
+void GUIComponentAdapter::addEventHandler(sf::RenderWindow &window, sf::Event event) {
+
+}
+
+void GUIComponentAdapter::update() {
+
+}
